8cf71b9c47f9e9dcfd40f09c518bd639e55c8354
Zach Underwood <zawood11@gmail.com>
2022-04-23 22:41:53 -0400

    completed assignment

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,8 +1,19 @@
 class MoviesController < ApplicationController
-  
+
+  def create
+    movie = Movie.create(movie_params)
+    render json: movie, status: :created
+  end
+
   def index
     movies = Movie.all
     render json: movies
   end
 
+  private
+  
+  def movie_params
+    params.permit(:title, :year, :length, :director, :description, :poster_url, :category, :discount, :female_director)
+  end
+  
 end
